a2b9b43709e39dd87fafc80763f2b6cf00d71e34
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-24 19:25:22 +0300

    Initial Commit

---
+++ ColorPickerInSwiftUI/ColorPickerInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerInSwiftUIApp.swift
+//  ColorPickerInSwiftUI
+//
+//  Created by Ramill Ibragimov on 24.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
